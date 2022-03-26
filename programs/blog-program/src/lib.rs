53e4672f1ad628b5189b94ceaf8a9b94bbba0b05
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-03-26 09:56:28 +0100

    blog_program done (quiero agregar CPIs)

--- programs/blog-program/src/lib.rs
+++ programs/blog-program/src/lib.rs
@@ -11,14 +11,44 @@ pub mod blog_program {
     pub fn initialize_blog(
         // El primer parámetro para cada RPC handler/instrucción es el Context struct
         ctx: Context<InitializeBlog>, 
-        blog_account_bump: u8 
+        blog_account_bump: u8,
+        vector_capacity: u16,
+        category: String,
     ) -> Result<()> {
 
         let blog = &mut ctx.accounts.blog_account; // declaramos una refecencia mutable del account
 
         blog.bump = blog_account_bump; // Almacenamos el bump!
         blog.authority = *ctx.accounts.authority.to_account_info().key; // * actúa para deshacer la referencia, es lo contrario a &
+                // simplemente con este * nos aseguramos de que estamos guardando el valor en vez del puntero (esto es Rust)
         blog.post_count = 0;
+        blog.category = category;
+        blog.vector_capacity = vector_capacity;
+        blog.subscribers_pubkeys = Vec::new(); // Así es como se inicializa un vector
+
+        Ok(())
+    }
+
+    pub fn update_blog(
+        ctx: Context<InitializeBlog>, 
+        category: String,
+    ) -> Result<()> {
+
+        let blog = &mut ctx.accounts.blog_account;
+
+        blog.category = category;
+
+        Ok(())
+    }
+
+    pub fn add_subscriber(
+        ctx: Context<InitializeBlog>, 
+        sub_pubkey: Pubkey,
+    ) -> Result<()> {
+
+        let blog = &mut ctx.accounts.blog_account;
+
+        blog.subscribers_pubkeys.push(sub_pubkey); // Añadimos la pubkey del subscriptor a nuestro vector
 
         Ok(())
     }
@@ -57,21 +87,36 @@ pub mod blog_program {
 
         Ok(())
     }
+
+    pub fn delete_post(
+        ctx: Context<UpdatePost>, 
+        title: String,
+        body: String
+    ) -> Result<()> {
+
+        let post = &mut ctx.accounts.post_account;
+
+        post.title = title;
+        post.body = body;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 // Puedes acceder a los argumentos de las instruccion con #[instruction(..)]
 // Tiene que ser el mismo orden que en la instrucción, se suele usar para almacenar el bump 
 // y para establecer el espacio que ocupa variables dinámicas (String y Vectores)
-#[instruction(blog_account_bump: u8)]
+#[instruction(blog_account_bump: u8, vector_capacity: u16, category: String)]
 pub struct InitializeBlog<'info> {
     #[account(
         init, // Permite inicializar la Account
         payer = authority, // Especifíca quien va a pagar el rent
-        space = 8 /*discriminator*/ + 1 /*u8*/ + 1 /*u8*/ + 32 /*Pubkey*/,
-        seeds = [ // Especifíca y comprueba las seeds para el PDA de la Account con init, 
+        space = BlogAccount::space(vector_capacity, &category),
+        seeds = [ // Nuestra Account tendrá un PDA derivado de las seeds "blog" + authority pubkey
+            // Especifíca y comprueba las seeds para el PDA de la Account con init, 
             // combinado con init comprueba si el PDA ya existe
-            b"blog".as_ref(),
+            b"blog".as_ref(), // as_ref() es un método que convierte código genérico en una referencia
             authority.key().as_ref(), // Esto es Rust: as_ref() es un método que nos permite pedir prestado una referencia
             // Léete esta capítulo del libro de Rust: https://doc.rust-lang.org/book/ch04-00-understanding-ownership.html
         ],
@@ -88,24 +133,40 @@ pub struct InitializeBlog<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(post_account_bump: u8, title: String, body: String, vector_capacity: u16)]
+pub struct UpdateBlog<'info> {
+    #[account(mut, has_one = authority)]
+    blog_account: Account<'info, BlogAccount>,
+    #[account(mut)]
+    authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct AddSubscriber<'info> {
+    #[account(mut, has_one = authority)]
+    blog_account: Account<'info, BlogAccount>,
+    #[account(mut)]
+    authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(post_account_bump: u8, title: String, body: String)]
 pub struct CreatePost<'info> {
     #[account(
         init, 
         payer = authority, 
-        seeds = [
-            b"post".as_ref(), // as_ref() es un método que convierte código genérico en una referencia
+        seeds = [ // cada post tendrá un PDA derivado de ["post" + Account Blog pubkey (que tmb es un PDA) + post_count]
+            b"post".as_ref(), 
             blog_account.key().as_ref(),
             &[blog_account.post_count as u8].as_ref(),
         ],
         bump, 
-        space = PostAccount::space(&title, &body, vector_capacity), // Dice cuando espacio va a ocupar
+        space = PostAccount::space(&title, &body), // Dice cuando espacio va a ocupar
                     // la Account, necesario para calcular la cantidad de rent-exemption
                     // llamamos a la función space
     )]
     post_account: Account<'info, PostAccount>,
     // Todas las validaciones son manejadas por #[account(..)] macros, permitiéndonos centrarnos en la lógica o en el diseño de las instrucciones
-    #[account(mut, has_one = authority)] //
+    #[account(mut, has_one = authority)] // has_one = nos permite implementar una restricción en la account para que solo el usuario pueda modificarla
     blog_account: Account<'info, BlogAccount>,
     #[account(mut)]
     authority: Signer<'info>,
@@ -113,10 +174,15 @@ pub struct CreatePost<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(title: String, body: String)]
 pub struct UpdatePost<'info> {
     #[account(mut, has_one = authority)]
-    blog_account: Account<'info, BlogAccount>,
+    post_account: Account<'info, PostAccount>,
+    #[account(mut)]
+    authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct DeletePost<'info> {
     #[account(mut, has_one = authority)]
     post_account: Account<'info, PostAccount>,
     #[account(mut)]
@@ -127,11 +193,29 @@ pub struct UpdatePost<'info> {
 /// Definimos un struct que tiene tres propiedades públicas: bump, post_count y authority
 /// Esta Account será pasada como argumento a las instrucciones si la incluimos en el Context
 #[account]
-#[derive(Default)] // Ya que no tenemos ninguna variable dinámica (String, Vector), incluimos este macro
 pub struct BlogAccount {
     pub bump: u8, // int de 1byte/8bits, bump: Nos dice el valor que completa a las seeds para obtener un PDA válido
     pub post_count: u8,  // int para almacenar el número de posts que tiene cada blog
     pub authority: Pubkey, // almacenamos la Pubkey del usuario propietario del blog
+    pub category: String, // almacenamos el tema/categoria sobre el blog del usuario
+    pub vector_capacity: u16, // Guarmos la capacidad máxima del vector
+    pub subscribers_pubkeys: Vec<Pubkey>, // Guardamos las pubKeys de los usuarios que estén suscritos al blog
+}
+
+impl BlogAccount { // Cada struct puede tener múltiple impl blocks, básicamente puedes declarar funciones
+    pub fn space (capacity: u16, category: &str) -> usize { // Esta función devuelve un integer unsigned (sólo permite positivos)
+        // los strings son referencias, léete esta capítulo del libro de Rust: https://doc.rust-lang.org/book/ch04-00-understanding-ownership.html
+        8 + // Discrimanator: son 8 bytes que Anchor pone al frente de una cuenta, como una cabecera. Permite a Anchor saber 
+        // qué tipo de cuenta debe deserializar los datos.
+        1 + // u8
+        32 + // Pubkey
+        4 + category.len() + // el 4 es otro discriminator, + la longitud de la cadena, esto genera un problema, una vez que la Account es creada
+        // el usuario no podría poner una categoría más larga de lo que puso en un primer momento, esto se podría arreglar especificando un 
+        // espacio extra, pero como esto es un ejemplo tampoco es necesario optimizarlo tanto, pero haré un test que compruebe esta situación
+        4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
+        // Para sacar el espacio que ocupa un vector tenemos que multiplacar la capacidad que asignamos nosotros por el espacio de la variable que 
+        // queremos vectorizar. La capacidad representa cuantas posiciones podemos almacenar dentro del vector.
+    }
 }
 
 
@@ -142,23 +226,15 @@ pub struct PostAccount{
     pub entry: u8,
     pub title: String,
     pub body: String,
-    pub vector_capacity: u16, // Guarmos la capacidad máxima del vector
-    pub likes_pubkeys: Vec<Pubkey>, // Guardamos las pubKeys de los usuarios que les guste el post
 }
 
-impl PostAccount{ // Cada struct puede tener múltiple impl blocks, básicamente puedes declarar funciones
-    pub fn space(title: &str, body: &str, capacity: u16) -> usize{ // Esta función devuelve un integer unsigned (sólo permite positivos)
-        // los strings son referencias, léete esta capítulo del libro de Rust: https://doc.rust-lang.org/book/ch04-00-understanding-ownership.html
-        8 + // Discrimanator: son 8 bytes que Anchor pone al frente de una cuenta, como una cabecera. Permite a Anchor saber 
-            // qué tipo de cuenta debe deserializar los datos.
+impl PostAccount{ 
+    pub fn space(title: &str, body: &str) -> usize{ 
+        8 + // discriminator
         32 + // Pubkey
         1 + // u8
         1 + // u8
-        4 + title.len() + // el 4 es otro discriminator + la longitud de la cadena, esto genera un problema, una vez que la Account es creada
-                        // el usuario no podría poner un título más largo de lo que puso en un primer momento
-        4 + body.len() +
-        4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
-        // Para sacar el espacio que ocupa un vector tenemos que multiplacar la capacidad que asignamos nosotros por el espacio de la variable que 
-        // queremos vectorizar. La capacidad representa cuantas posiciones podemos almacenar dentro del vector.
+        4 + title.len() + // String
+        4 + body.len() // String
     }
 }
